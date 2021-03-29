66db7b1999be6f4b579ebbac34c545fde31938a4
kantaro <kantarodev@gmail.com>
2021-03-29 15:27:14 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner } from "@chakra-ui/react";
 import { Wrap, WrapItem } from "@chakra-ui/layout";
-import { memo, VFC } from "react";
+import { memo, useEffect, VFC } from "react";
+import { UseAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = UseAllUsers();
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="名前"
-          fullName="firstName lastName"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
